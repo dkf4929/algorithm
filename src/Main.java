8040db4b5c7fb2fc7eed2e0b547dd2bcce382e85
dkf4929 <sng4929@naver.com>
2023-05-27 18:42:46 +0900

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import lv2.rate70.JadenCaseString;
+
+public class Main {
+    public static void main(String[] args) {
+        JadenCaseString s = new JadenCaseString();
+        s.solution("me mea me   ");
+    }
+}
