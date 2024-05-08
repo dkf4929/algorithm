4b238baea01ac3ec2bcb00d78a717b0c410d5748
yeonghoS <dkf4928@naver.com>
2024-05-08 13:28:56 +0900

    feat: lv1.붕대감기

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import lv1.DataAnalyze;
+import lv1.Bandaging;
 
 public class Main {
     public static void main(String[] args) {
-        DataAnalyze s = new DataAnalyze();
-        s.solution(new int[][]{{1, 20300104, 100, 80}, {2, 20300804, 847, 37}, {3, 20300401, 10, 8}},"date",20300501,	"remain");
+        Bandaging solution = new Bandaging();
+        solution.solution(new int[]{5, 1, 5}, 30, new int[][]{{2, 10}, {9, 15}, {10, 5}, {11, 5}});
     }
 }
