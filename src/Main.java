f2477c50b98f9845d4a711afcc6f07760e330260
dkf4929 <64400921+dkf4929@users.noreply.github.com>
2024-05-08 13:39:04 +0900

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import lv1.Bandaging;
-
-public class Main {
-    public static void main(String[] args) {
-        Bandaging solution = new Bandaging();
-        solution.solution(new int[]{5, 1, 5}, 30, new int[][]{{2, 10}, {9, 15}, {10, 5}, {11, 5}});
-    }
-}
